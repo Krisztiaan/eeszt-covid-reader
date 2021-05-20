75b523d93c434da5b842f42ebd1ecc02b13bd0e1
Krisztiaan <krisz011@gmail.com>
2021-05-20 02:23:43 +0200

    Add Sentry

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
 import 'react-native-reanimated';
+import * as Sentry from 'sentry-expo';
 
 import AppRoot from './src/AppRoot';
+
+Sentry.init({
+  dsn: 'https://c53b766227be407281d97d5a8098cc32@o691735.ingest.sentry.io/5774622',
+  enableInExpoDevelopment: false,
+  debug: false,
+  integrations: [new Sentry.Native.ReactNativeTracing()],
+});
 
 export default AppRoot;
