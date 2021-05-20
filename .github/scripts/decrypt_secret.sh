d1f118aa594011afcd585305b698f94c98a911e6
Krisztiaan <krisz011@gmail.com>
2021-05-20 05:43:44 +0200

    Change pg to pc file name

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $GITHUB_WORKSPACE/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PG_PASSPHRASE" \
---output $GITHUB_WORKSPACE/secrets/pg.json $GITHUB_WORKSPACE/pg.json.gpg
+--output $GITHUB_WORKSPACE/secrets/pc.json $GITHUB_WORKSPACE/pc.json.gpg
